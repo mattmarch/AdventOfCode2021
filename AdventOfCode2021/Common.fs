9639b5ceb06102cd045d3f5fd36ec3b61aeda1b8
Matt March <matthew.march@ghyston.com>
2021-12-02 09:34:58 +0000

    Whitespace changes

--- AdventOfCode2021/Common.fs
+++ AdventOfCode2021/Common.fs
@@ -6,9 +6,9 @@ open System.IO
 let readLines path: string seq = File.ReadLines path
 
 let splitBy (separator: string) (inputString: string): string list = 
-  inputString.Split([|separator|], StringSplitOptions.None) |> Array.toList
+    inputString.Split([|separator|], StringSplitOptions.None) |> Array.toList
 
 let unpack2 l =
-  match l with
-  | [a; b] -> a, b
-  | _ -> failwithf "Tried to unpack2 list without exactly 2 elements: %A" l
+    match l with
+    | [a; b] -> a, b
+    | _ -> failwithf "Tried to unpack2 list without exactly 2 elements: %A" l
