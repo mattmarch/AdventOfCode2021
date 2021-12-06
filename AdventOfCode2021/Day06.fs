77cb1ed1febdf8b3e4bb70177c3524ce933148e7
Matt March <matthew.march@ghyston.com>
2021-12-06 09:03:12 +0000

    Formatting changes

--- AdventOfCode2021/Day06.fs
+++ AdventOfCode2021/Day06.fs
@@ -19,15 +19,24 @@ let parseInput input =
 let runStep previousState =
     let newBirths = previousState |> List.item 0
     [
-        previousState |> List.item 1; // 0
-        previousState |> List.item 2; // 1
-        previousState |> List.item 3; // 2
-        previousState |> List.item 4; // 3
-        previousState |> List.item 5; // 4
-        previousState |> List.item 6; // 5
-        newBirths + (previousState |> List.item 7) // 6
-        previousState |> List.item 8 // 7
-        newBirths // 8
+        // 0
+        previousState |> List.item 1
+        // 1
+        previousState |> List.item 2
+        // 2
+        previousState |> List.item 3
+        // 3
+        previousState |> List.item 4
+        // 4
+        previousState |> List.item 5
+        // 5
+        previousState |> List.item 6
+        // 6
+        newBirths + (previousState |> List.item 7)
+        // 7
+        previousState |> List.item 8
+        // 8
+        newBirths
     ]
 
 let rec runSteps numSteps startState =
