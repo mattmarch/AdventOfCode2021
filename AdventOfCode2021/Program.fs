3900c85edcfcd710fd9c1a08200bc460ad9171ef
Matt March <matthew.march@ghyston.com>
2021-12-17 17:23:41 +0000

    Make it possible to run a single day when running full project

--- AdventOfCode2021/Program.fs
+++ AdventOfCode2021/Program.fs
@@ -1,27 +1,45 @@
 ﻿open System.Diagnostics
 
-let printDay number solveA solveB getInput =
-    let stopWatch = Stopwatch.StartNew()
-    let input = getInput ()
-    let partA = solveA input
-    let partB = solveB input
-    let elapsedMs = stopWatch.Elapsed.TotalMilliseconds
-    printfn $"Day %i{number}\t part 1 = %15O{partA}\t\t part 2 = %15O{partB}\t\t in %f{elapsedMs}ms"
+type SelectedDay =
+    | AllDays
+    | SingleDay of int
 
-printDay 1 Day01.solveA Day01.solveB Day01.getInput
-printDay 2 Day02.solveA Day02.solveB Day02.getInput
-printDay 3 Day03.solveA Day03.solveB Day03.getInput
-printDay 4 Day04.solveA Day04.solveB Day04.getInput
-printDay 5 Day05.solveA Day05.solveB Day05.getInput
-printDay 6 Day06.solveA Day06.solveB Day06.getInput
-printDay 7 Day07.solveA Day07.solveB Day07.getInput
-printDay 8 Day08.solveA Day08.solveB Day08.getInput
-printDay 9 Day09.solveA Day09.solveB Day09.getInput
-printDay 10 Day10.solveA Day10.solveB Day10.getInput
-printDay 11 Day11.solveA Day11.solveB Day11.getInput
-printDay 12 Day12.solveA Day12.solveB Day12.getInput
-printDay 13 Day13.solveA Day13.solveB Day13.getInput
-printDay 14 Day14.solveA Day14.solveB Day14.getInput
-printDay 15 Day15.solveA Day15.solveB Day15.getInput
-printDay 16 Day16.solveA Day16.solveB Day16.getInput
-printDay 17 Day17.solveA Day17.solveB Day17.getInput
+let printDayIfChosen chosenDay number solveA solveB getInput =
+    match chosenDay with
+    | SingleDay d when number <> d -> ()
+    | _ ->
+        let stopWatch = Stopwatch.StartNew()
+        let input = getInput ()
+        let partA = solveA input
+        let partB = solveB input
+        let elapsedMs = stopWatch.Elapsed.TotalMilliseconds
+        printfn $"Day %i{number}\t part 1 = %15O{partA}\t\t part 2 = %15O{partB}\t\t in %f{elapsedMs}ms"
+
+[<EntryPoint>]
+let main argv =
+    let dayToRun =
+        if argv.Length > 0 then
+            SingleDay (argv.[0] |> int)
+        else
+            AllDays
+    let printDay number solveA solveB getInput = printDayIfChosen dayToRun number solveA solveB getInput
+    
+    printDay 1 Day01.solveA Day01.solveB Day01.getInput
+    printDay 2 Day02.solveA Day02.solveB Day02.getInput
+    printDay 3 Day03.solveA Day03.solveB Day03.getInput
+    printDay 4 Day04.solveA Day04.solveB Day04.getInput
+    printDay 5 Day05.solveA Day05.solveB Day05.getInput
+    printDay 6 Day06.solveA Day06.solveB Day06.getInput
+    printDay 7 Day07.solveA Day07.solveB Day07.getInput
+    printDay 8 Day08.solveA Day08.solveB Day08.getInput
+    printDay 9 Day09.solveA Day09.solveB Day09.getInput
+    printDay 10 Day10.solveA Day10.solveB Day10.getInput
+    printDay 11 Day11.solveA Day11.solveB Day11.getInput
+    printDay 12 Day12.solveA Day12.solveB Day12.getInput
+    printDay 13 Day13.solveA Day13.solveB Day13.getInput
+    printDay 14 Day14.solveA Day14.solveB Day14.getInput
+    printDay 15 Day15.solveA Day15.solveB Day15.getInput
+    printDay 16 Day16.solveA Day16.solveB Day16.getInput
+    printDay 17 Day17.solveA Day17.solveB Day17.getInput
+    
+    0 
--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 My solutions to [Advent of Code 2021](https://adventofcode.com/2021) in F#.
 
-Run all the solutions with:
+Calculate all the answers with:
 ```
 dotnet run --project AdventOfCode2021
+```
+
+Calculate a single answer with (e.g. for question 6):
+```
+dotnet run --project AdventOfCode2021 6
 ```
 
 Run unit tests:
